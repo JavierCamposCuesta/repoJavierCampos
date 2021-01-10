086769c8fedec01904fc7b7d6241723ff1e3e6b2
JavierCampos <javiercamposcuesta@gmail.com>
2021-01-10 21:37:34 +0100

    10/01/2021

--- Programacion1/Python/PruebaEclipseSusMueertos/Avesifunciona.py
+++ Programacion1/Python/PruebaEclipseSusMueertos/Avesifunciona.py
@@ -3,3 +3,17 @@ Created on 3 dic. 2020
 
 @author: Javier
 '''
+c=25
+c1=1
+e=" "
+a="*"
+s="+"
+for i in range (1,c+1):
+    print((e*(c))+s*(c1))
+    c-=1
+    c1+=2
+for i in range (1,5):
+    print(e*int((c1/2)-2)+"|"*8)
+    
+    
+    
