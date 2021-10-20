7926d34fdcb35faf001a2efdaef03901a06fd8ea
JavierCamposCuesta <javiercamposcuesta@gmail.com>
2021-10-20 09:37:14 +0200

    actualizo

---
+++ 2ºDaw/DWEC/ProyectoBlog/addPost.js
@@ -0,0 +1,77 @@
+let form = document.getElementById("form");
+form.addEventListener("submit", (e)=>{
+    e.preventDefault(); subirPost()});
+
+let boton = document.getElementById("boton");
+let titulo = document.getElementById("titulo");
+let cuerpoPost = document.getElementById("cuerpoPost");
+let listaUsuarios = document.getElementById("listaUsuarios");
+
+//Leemos para hacer la seleccion de todos los perfiles
+let peticion = new XMLHttpRequest();
+peticion.open('GET', "http://localhost:3000/profile");
+peticion.send();
+peticion.addEventListener('readystatechange', function() {
+    console.log("Estado de la petición: " + peticion.readyState);
+    if (peticion.readyState === 4) {
+        if (peticion.status === 200) {
+            console.log("Datos recibidos:");
+            
+            
+            
+            let usuarios = JSON.parse(peticion.responseText); // Convertirmos los datos JSON a un objeto
+            let listaUsuarios = document.getElementById("listaUsuarios");
+            
+            
+            for(i = 0; i< usuarios.length; i++){
+        let option = document.createElement('option');
+        let valor = (i+1);
+        let texto = usuarios[i].name;
+        option.value=valor;
+        option.text=texto;
+        listaUsuarios.appendChild(option);
+        let indiceOption = 0;
+
+       
+       
+    }
+    
+    console.log(usuarios);
+    } else {
+    console.log("Error " + peticion3.status + " (" + peticion3.statusText + ") en la petición");
+    }
+    }
+    }); 
+
+//Creamos un evento para cuando cambie la seleccion de usuario
+listaUsuarios.addEventListener("change", ()=>{indiceOption=listaUsuarios.selectedIndex;});
+
+function subirPost(){
+    //Evaluamos que el campo del titulo y el cuerpo no estén vacios
+    if(titulo.value.length>0 && cuerpoPost.value.length>0){
+        const fecha = new Date();
+        const newPost={
+            title: titulo.value,
+            body: cuerpoPost.value,
+            author: listaUsuarios.options[indiceOption].text,
+            date: fecha.toLocaleDateString()
+    }
+    const peticion2=new XMLHttpRequest();
+        peticion2.open('POST', 'http://localhost:3000/posts');
+        peticion2.setRequestHeader('Content-type', 'application/json');  // Siempre tiene que estar esta línea si se envían datos
+        peticion2.send(JSON.stringify(newPost));              // Hay que convertir el objeto a una cadena de texto JSON para enviarlo
+
+        //Recargamos la pagina y quitamos la clase oculto para que se pueda ver el mensaje de enviado correctamente
+        peticion2.addEventListener('load', ()=>{
+            let mensajeEnviado=document.getElementById("mensajeEnviado");
+            mensajeEnviado.classList.remove("oculto");
+        })  //Comprueba que el formulario se ha enviado y luego ejecuta el cambio de pagina
+            console.log("Formulario enviado")
+}
+else{
+    alert("Los campos no pueden estar vacios");
+    let mensajeEnviado=document.getElementById("mensajeEnviado");
+            mensajeEnviado.classList.add("oculto");
+}
+}
+
--- 2ºDaw/DWEC/ProyectoBlog/post.js
+++ 2ºDaw/DWEC/ProyectoBlog/post.js
@@ -1,6 +1,6 @@
 let titulo = document.getElementById("titulo");
 let cuerpo = document.getElementById("cuerpo");
-let fechaCreacion = document.getElementById("fechaCreacion");
+let fechaCreacion = document.getElementById("fecha");
 let autor = document.getElementById("autor");
 
 
@@ -33,6 +33,7 @@ let usuarios = JSON.parse(peticion.responseText); // Convertirmos los datos JSON
 titulo.innerHTML=usuarios.title;
 cuerpo.innerHTML=usuarios.body;
 autor.innerHTML=usuarios.author;
+fechaCreacion.innerHTML=usuarios.date;
 
 console.log(usuarios);
 } else {
@@ -77,7 +78,6 @@ peticion2.addEventListener('readystatechange', function() {
 
 
 //Leemos para hacer la seleccion de todos los perfiles
-let listaComentadores = document.getElementById("listaComentadores");
 
 let peticion3 = new XMLHttpRequest();
 peticion3.open('GET', "http://localhost:3000/profile");
@@ -85,18 +85,23 @@ peticion3.send();
 peticion3.addEventListener('readystatechange', function() {
     console.log("Estado de la petición: " + peticion3.readyState);
     if (peticion3.readyState === 4) {
-    if (peticion3.status === 200) {
-    console.log("Datos recibidos:");
+        if (peticion3.status === 200) {
+            console.log("Datos recibidos:");
+            
+            
+            
+            let usuarios = JSON.parse(peticion3.responseText); // Convertirmos los datos JSON a un objeto
+            let listaComentadores = document.getElementById("listaComentadores");
+            
+            
+            for(i = 0; i< usuarios.length; i++){
+        let option = document.createElement('option');
+        let valor = (i+2);
+        let texto = usuarios[i].name;
+        option.value=valor;
+        option.text=texto;
+        listaComentadores.appendChild(option);
 
-    
-
-    let usuarios = JSON.parse(peticion3.responseText); // Convertirmos los datos JSON a un objeto
-    
-    
-    for(i = 0; i< usuarios.length; i++){
-        let fila = '<option value='+(i+2)+'>'+usuarios[i].name+'</option>';
-        console.log(fila);
-        listaComentadores.insertAdjacentElement(fila);
        
        
     }
@@ -106,4 +111,42 @@ peticion3.addEventListener('readystatechange', function() {
     console.log("Error " + peticion3.status + " (" + peticion3.statusText + ") en la petición");
     }
     }
-    });    
+    }); 
+    
+    //Subimos el comentario---------------------------------
+    let form = document.getElementById("form");
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        subirComentario()});
+
+    let boton = document.getElementById("boton");
+    let comentario = document.getElementById("comentario");
+    let listaComentadores = document.getElementById("listaComentadores");
+    let indiceOption = 0;
+
+    //Creamos un evente para cuando se cambie la seleccion del autor
+    listaComentadores.addEventListener("change", ()=>{indiceOption=listaComentadores.selectedIndex;});
+    
+    function subirComentario(){
+        //Evaluamos que el campo del comentario no esté vacio
+        if(comentario.value.length>0){
+            const newComment={
+                body: comentario.value,
+                nick: listaComentadores.options[indiceOption].text,
+                //Utilizamos el idParametro que nos tragimos al principio
+                postId: parseInt(idParametro)
+                
+            } 
+            const peticion4=new XMLHttpRequest();
+            peticion4.open('POST', 'http://localhost:3000/comments');
+            peticion4.setRequestHeader('Content-type', 'application/json');  // Siempre tiene que estar esta línea si se envían datos
+            peticion4.send(JSON.stringify(newComment));              // Hay que convertir el objeto a una cadena de texto JSON para enviarlo
+            peticion4.addEventListener('load', ()=>{location.reload()})  //Comprueba que el formulario se ha enviado y luego ejecuta el cambio de pagina
+            console.log("Formulario enviado")
+        }
+        else{
+            alert("El comentario no puede estar vacio")
+        }
+    }
+   
+    
